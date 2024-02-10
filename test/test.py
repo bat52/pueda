eba35b5a6f9138d7a026c7c31aac7e9b74b63ef4
Marco Merlin <marcomerli@gmail.com>
2024-02-10 18:27:29 +0100

    fixed lint on test

--- test/test.py
+++ test/test.py
@@ -1,27 +1,32 @@
 #!/usr/bin/env python3
 
+import unittest
+
 import sys
 import os
 sys.path.append( os.path.join(os.path.dirname(__file__), '../src') )
 
-import unittest
-
+from pueda.edalize import icarus
+from pueda.pyverilator import pyverilator_wrapper
+from test_verilator_counter import test_verilator_counter
+from pueda.yosys import yosys
 class TestStringMethods(unittest.TestCase):
+    """ Unit test class """
 
     def test_edalize_icarus_hello(self):
-        from pueda.edalize import icarus        
+        """ test edalize icarus hello """
         icarus(simname='hello', top='hello_tb', src_dirs = ['./hello'], dump_en=False)
 
     def test_edalize_icarus_counter(self):
-       from pueda.edalize import icarus
-       icarus(simname='counter', top='counter_tb',
-              src_dirs = ['./counter/rtl','./counter/tb'],
-              iverilog_options=['-g2005-sv'],
-              dump_en=False)
-       # self.assertEqual('foo'.upper(), 'FOO')
+        """ test edalize icarus hello """
+        icarus(simname='counter', top='counter_tb',
+            src_dirs = ['./counter/rtl','./counter/tb'],
+            iverilog_options=['-g2005-sv'],
+            dump_en=False)
+        # self.assertEqual('foo'.upper(), 'FOO')
 
     def test_pyverilator_counter(self):
-        from pueda.pyverilator import pyverilator_wrapper
+        """ test pyverilator counter """
         pv = pyverilator_wrapper(fname='./counter/rtl/counter.v',
                                  src_dirs=['./counter/rtl'],
                                  command_args = [],
@@ -34,11 +39,11 @@ class TestStringMethods(unittest.TestCase):
         pv.view_waves(mode='vcdterm', block_en=False)
 
     def test_verilator_counter(self):
-        from test_verilator_counter import test_verilator_counter
+        """ test verilator counter """
         test_verilator_counter()
 
     def test_yosys_counter(self):
-        from pueda.yosys import yosys
+        """ test yosys counter """
         yosys(top='counter', src_dirs = ['./counter/rtl'], synth_en=True)
 
 if __name__ == '__main__':
