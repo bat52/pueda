ab1dd6584d7cd8c75931c06cf618a2e2c6570dac
Marco Merlin <marcomerli@gmail.com>
2024-02-11 00:07:51 +0100

    more pylint fixes

--- src/pueda/common.py
+++ src/pueda/common.py
@@ -5,14 +5,17 @@ A collection of Python tools for micro-Electronics Design Automation. """
 
 import os
 import shutil
+import wget
 
 # load vcd_view for backwards compatibility
 from pueda.vcd import vcd_view
 
-def list2str(l,sep=' '):
+def list2str(l,sep=' ') -> str:
+    """ Convert a list into a string """
     return sep.join(str(s) for s in l)
 
 def get_source_files_alldir(dirlist,fmts=['.v','.sv','.vh'], excludes = []) -> list:
+    """ Gets source files list, recursive to all directories """
     files = []
     # create files list
     for d in dirlist:
@@ -21,6 +24,7 @@ def get_source_files_alldir(dirlist,fmts=['.v','.sv','.vh'], excludes = []) -> l
     return files
 
 def get_source_files(directory,fmts=['.v','.sv','.vh'], excludes = []) -> list:
+    """ Get source files in a specific directory """
     # print(directory)
 
     if os.path.isdir(directory):
@@ -41,9 +45,8 @@ def get_source_files(directory,fmts=['.v','.sv','.vh'], excludes = []) -> list:
                 # print(ldir)
                 llist = get_source_files( ldir ,fmts=fmts )
                 foutlist = foutlist + llist
-            pass
     else: # if os.path.isdir(directory):
-        print('%s : WARNING!!! directory "%s" does not exist!' % (__name__, directory) )
+        print(f'{__name__} : WARNING!!! directory "{directory}" does not exist!')
         foutlist = []
 
     # print(foutlist)
@@ -51,6 +54,7 @@ def get_source_files(directory,fmts=['.v','.sv','.vh'], excludes = []) -> list:
     return foutlist
 
 def get_inc_list(inclist,work_root='',prefix='-I') -> list:
+    """ get the list of include directories """
 
     outlist = []
     for ipath in inclist:
@@ -61,12 +65,10 @@ def get_inc_list(inclist,work_root='',prefix='-I') -> list:
         outlist.append(inc)
 
     return outlist
-    pass
 
 def get_remote_files(url,flist=[],dstdir='./work_get'):
-    import wget
-
-    if not(os.path.isdir(dstdir)):
+    """ Get files in a remote location """
+    if not os.path.isdir(dstdir):
         os.makedirs(dstdir)
 
     if len(flist)>0:
@@ -75,41 +77,41 @@ def get_remote_files(url,flist=[],dstdir='./work_get'):
             dst = os.path.join(dstdir,f)
             # print(src)
             # print(dst)
-            if not(os.path.isfile(dst)):
+            if not os.path.isfile(dst):
                 wget.download( src , dst )
     else:
         # single file
         wget.download( url , dstdir )
         fext = os.path.splitext(url)[1]
         if fext == '.zip':
-            os.system('cd %s && dtrx -f `ls *.zip && cd ..`' % dstdir)
+            os.system(f'cd {dstdir} && dtrx -f `ls *.zip && cd ..`')
 
-def get_work_path(tool=''):
+def get_work_path(tool='') -> str:
+    """ Get the work folder path """
     return os.path.join(os.getcwd() , 'work_' + tool)    
 
-def work_exists(tool=''):
+def work_exists(tool='') -> bool:
+    """ Check if work directory exists """
     return os.path.isdir(get_work_path(tool))
 
-def get_clean_work(tool='',makedir=False,rm_en=True):
-    work_root = get_work_path(tool) 
+def get_clean_work(tool='',makedir=False,rm_en=True) -> str:
+    """ get a work folder, create if not exists """
+    work_root = get_work_path(tool)
 
     if work_exists(tool) and rm_en:
         # delete work directory
         shutil.rmtree(work_root,ignore_errors=True)
-    
     if makedir:
         os.makedirs(work_root)
-        
     return work_root
 
 def write_file_lines(fname, lines=[], mode='w', print_en=False):
-    
-    f = open(fname, mode)
-    for l in lines: 
-        # print(l)
-        f.write(l + '\n')
-    f.close()
+    """ Write the specified lines to a file """
+    with open(fname, mode, encoding='utf-8') as f:
+        for l in lines:
+            # print(l)
+            f.write(l + '\n')
 
     # print file
     if print_en:
-        os.system('cat %s' % fname)
+        os.system(f'cat {fname}' % fname)
